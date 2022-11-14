f68069f51ace39089ba9f712fc50beaa14dbdc83
Chameleon Cloud User <cc@icc2023-tacc.novalocal>
2022-11-14 19:36:59 +0000

    added Chameleon real transfer for all 4 cases -GD,RL,BO,and NO

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 configurations = {
     "receiver": {
-        "host": "128.205.218.120",
+        "host": "192.5.86.238",
         "port": 5028
     },
-    "data_dir": "/home/jamilm/Documents/files_to_send/",#files_bash
+    "data_dir": "/home/cc/files_to_send/",#files_bash
     "bayes": {
         "initial_run": 3,
         "num_of_exp": -1
@@ -13,6 +13,6 @@ configurations = {
     "K": 1.02,
     "loglevel": "info",
     "probing_sec": 3,
-    "multiplier": 128,
+    "multiplier": 1024,
     "max_cc":32,
 }
